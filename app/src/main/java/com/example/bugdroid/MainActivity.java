d7900b90e82fe45560e8dd61e47595a25cfc54bc
Emna-Charfi <79401218+Emna-Charfi@users.noreply.github.com>
2023-09-25 13:48:45 +0100

    first commit

---
+++ app/src/main/java/com/example/bugdroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bugdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.layout_linear);
+        setContentView(R.layout.layout_relative);
+    }
+}
